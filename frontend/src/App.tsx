5afc596e271c60468ec5584a4984ae366a0febb2
Jeffrey Zhang <jzh4ng@berkeley.edu>
2025-01-09 14:31:49 -0800

    create frontend files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import { Box } from '@mantine/core'
+// import { useState } from 'react'
+// import './App.css'
+import { Container } from '@mantine/core'
+import { Route, Routes } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import RecipePage from './pages/RecipePage';
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
-    <Box bg="red.5" my="xl" component="a" href="/">
-    My component
-    </Box>
-  )
+    <Container>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/recipe/:id' element={<RecipePage />} /> {/* This is a Dynamic Route */}
+      </Routes>
+    </Container>
+  );
 }
 
 export default App
