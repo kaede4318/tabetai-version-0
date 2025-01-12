f406ccfaf57f93cbe09ad06687cfff1d3fa54c42
Jeffrey Zhang <jzh4ng@berkeley.edu>
2025-01-11 21:54:11 -0800

    build RecipeCard

---
+++ frontend/src/components/RecipeCard.tsx
@@ -0,0 +1,38 @@
+// import React from 'react'
+import type { Recipe } from "../store/recipe";
+
+import { Card, Image, Text, Badge, Group } from '@mantine/core';
+
+const RecipeCard = ({ recipe }: {recipe: Recipe}) => {
+    return (
+        <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+                <Image
+                    src={recipe.image}
+                    height={200}
+                    alt="Food"
+                />
+            </Card.Section>
+    
+            <Group 
+                justify="space-between" 
+                mt="md" 
+                mb="xs"
+            >
+                <Text fw={500}>{recipe.name}</Text>
+            </Group>
+    
+            <Group
+                gap="xs"
+            >
+                {recipe.tags.map((tag: any) => (
+                    <Badge color="#10bcfc">
+                        {tag.tag}
+                    </Badge>
+                ))}
+            </Group>
+        </Card>
+    )
+}
+
+export default RecipeCard
